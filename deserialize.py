e9de70dd3e5dd36fc8c3fc9c88d45eacf315ed8d
Mikayla Edwards <mikaylaedwards19@gmail.com>
2020-07-19 22:07:58 -0400

    add get_default_args to utils for use in construction

--- deserialize.py
+++ deserialize.py
@@ -3,17 +3,12 @@ import os
 from pathlib import Path
 from models import RegressionMeta
 from sklearn.linear_model import LinearRegression
+from utils import get_default_args
 
 
 __file__ = '{}/deserialize.py'.format(os.getcwd())
 
 PARENT_DIRECTORY = Path(__file__).parent
-
-model_json = Path('{}/model_files/model.json'.format(PARENT_DIRECTORY))
-with open(model_json) as m:
-    model_dict = json.load(m)
-
-rm = RegressionMeta.parse_obj(model_dict)
 
 # TO-DO: Write class to override constructor of LinearRegression
 # TO-DO: Add req args to json 
@@ -28,21 +23,21 @@ class RegressionLoader(LinearRegression):
         ## set all params from kwargs (non positional)
   
     @classmethod
-    def from_json(cls, model_json, req_args):
+    def from_json(cls, model_json):
+        """
+        @param: model_json: json file with serialized model data
+        """
         with open(model_json) as m:
             model_dict = json.load(m)
         meta = RegressionMeta.parse_obj(model_dict)
-        req_args_ = {k:v for (k,v) in meta.params.items() if k in req_args}
-        non_req = {k:v for (k,v) in meta.params.items() if k not in req_args}
+        required_args = get_default_args(LinearRegression.__init__)
+        req_args_ = {k:v for (k,v) in meta.params.items() if k in required_args}
+        non_req = {k:v for (k,v) in meta.params.items() if k not in required_args}
         return cls(non_req,**req_args_)
 
 
-req_args = ['fit_intercept','normalize','copy_X','n_jobs']
-model = RegressionLoader.from_json(model_json= model_json, req_args=req_args)
-print(model.coef_)
-#print(dir(model))
+model_json = Path('{}/model_files/model.json'.format(PARENT_DIRECTORY))
 
-
-
-# md = {'fit_intercept': True, 'normalize': False, 'copy_X': True, 'n_jobs': None}
-# RegressionLoader(**md)
+#required_args = get_default_args(LinearRegression.__init__)
+model = RegressionLoader.from_json(model_json= model_json)
+print(dir(model))
