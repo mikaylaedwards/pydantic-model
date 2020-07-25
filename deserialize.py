579d81f58233d1e1da853b13f6923460f919b65f
Mikayla Edwards <mikaylaedwards19@gmail.com>
2020-07-25 14:54:50 -0400

    add redirect for results of running deserialize

--- deserialize.py
+++ deserialize.py
@@ -4,28 +4,34 @@ from pathlib import Path
 from models import RegressionMeta
 from sklearn.linear_model import LinearRegression
 from utils import get_default_args
+from contextlib import redirect_stdout
+import pprint
 
 
 __file__ = '{}/deserialize.py'.format(os.getcwd())
 
-PARENT_DIRECTORY = Path(__file__).parent
+MODEL_FILES_PATH = Path(__file__).parent / 'model_files'
 
-# TO-DO: Write class to override constructor of LinearRegression
-# TO-DO: Add req args to json 
 
 class RegressionLoader(LinearRegression):
-    
-    # Override LinearRegression constructor: In sklearn(0.23) these four arguments are required as positional arguments
-    ## use kwargs in version 0.25+
-    def __init__(self,kwargs, fit_intercept=True, normalize=False, copy_X=True, n_jobs=None):
-        LinearRegression.__init__(self, fit_intercept, normalize, copy_X, n_jobs)
-        self.coef_= kwargs.get('coef_')
-        ## set all params from kwargs (non positional)
-  
+
+    # Override LinearRegression constructor: args to constructor must be keyword for 0.25+
+    def __init__(self, args, fit_intercept=True, normalize=False, copy_X=True, n_jobs=None):
+        LinearRegression.__init__(self,
+                                  fit_intercept=fit_intercept,
+                                  normalize=normalize,
+                                  copy_X=copy_X,
+                                  n_jobs=n_jobs)
+        # set all params not passed to LinReg constructor; created in modeling process (e.g. coef_, intercept_, etc.)
+        for arg, value in args.items():
+            setattr(self, arg, value)
+
     @classmethod
     def from_json(cls, model_json):
-        """  Loads in json file containing the serialized 
-             RegressionMeta class converted to json in serialize.py. To address
+        """Loads in json file containing the serialized RegressionMeta class
+        converted to json in serialize.py. Separates required args in
+        LinearRegression construcor and passesthem to current class as keyword
+        args.
 
         @param: model_json: json file with serialized model data
         """
@@ -34,13 +40,21 @@ class RegressionLoader(LinearRegression):
         # re-construct instance of RegressionMeta from json
         meta = RegressionMeta.parse_obj(model_json)
         required_args = get_default_args(LinearRegression.__init__)
-        req_args_ = {k:v for (k,v) in meta.params.items() if k in required_args}
-        non_req = {k:v for (k,v) in meta.params.items() if k not in required_args}
-        return cls(non_req,**req_args_)
+        req_args_ = {k: v for (k, v) in meta.params.items()
+                     if k in required_args}
+        non_req_args_ = {
+            k: v for (k, v) in meta.params.items() if k not in required_args}
+        # print(non_req_args_)
+        return cls(non_req_args_, **req_args_)
 
 
-model_json = Path('{}/model_files/model.json'.format(PARENT_DIRECTORY))
+model_json = Path(f'{MODEL_FILES_PATH}/model.json')
 
-#required_args = get_default_args(LinearRegression.__init__)
-model = RegressionLoader.from_json(model_json= model_json)
-print(dir(model))
+model = RegressionLoader.from_json(model_json=model_json)
+
+with open(f'{MODEL_FILES_PATH}/deserialize_results.txt', 'w') as f:
+    with redirect_stdout(f):
+        print('Model parameters:')
+        pprint.pprint(model.__dict__)
+        print('Model methods:')
+        pprint.pprint([x for x in dir(model) if not (x.startswith('__') or x in vars(model))])
